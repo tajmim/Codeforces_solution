3695909293cd7dc84ad8a0080c6f7c51f420fa60
unknown <shamima.bhola@gmail.com>
2024-04-01 01:32:33 +0600

    initial commit

---
+++ Odd One Out(1915A).py
@@ -0,0 +1,16 @@
+for _ in range(int(input())):
+    arr = [int(x) for x in input().split()]
+    arr.sort()
+    if(arr[0]==arr[1]):
+        print(arr[2])
+    else:
+        print(arr[0])    
+
+
+# for _ in range(int(input())):
+#     arr = [int(x) for x in input().split()]
+#     arr.sort()
+#     if(arr[0]==arr[1]):
+#         print(arr[2])
+#     else:
+#         print(arr[0])
